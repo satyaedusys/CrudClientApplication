7be407fa7b69f289d816581d67a10669f1cd1bdb
chandrakanta <edusyschandrakanta@gmail.com>
2022-12-27 15:00:46 +0530

    first commit

---
+++ CrudClientApplication/Controllers/CustomerController.cs
@@ -0,0 +1,163 @@
+﻿using CrudClientApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Security;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CrudClientApplication.Controllers
+{
+    
+    public class CustomerController : Controller
+    {
+        private readonly HttpClientHandler _clientHandler = new HttpClientHandler();
+        public CustomerController() 
+        {
+            _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, SslPolicyErrors) => { return true; };
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            GetAllCustResponse Customer = new GetAllCustResponse();
+            using (var httpclient=new HttpClient(_clientHandler))
+            {
+                httpclient.DefaultRequestHeaders.Clear();
+                httpclient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await httpclient.GetAsync("https://localhost:44301/api/Customer/get-all-customer");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent=await response?.Content?.ReadAsStringAsync();
+                    var responseList = JsonConvert.DeserializeObject<GetAllCustResponse>(responseContent);
+
+                    //List<CustomerModel> responseList = JsonConvert.DeserializeObject<GetAllCustResponse>/*(responseContent)?.CustomerList;*/
+                    Customer = responseList;  
+                }
+            }
+
+            return View(Customer);
+        }
+
+        [HttpGet]
+        [ActionName("Get-cust-ById")]
+        public async Task<IActionResult> GetCustById()
+        {
+
+            GetCustByIdCustResponse customer = new GetCustByIdCustResponse();
+            using (var httpclient = new HttpClient(_clientHandler))
+            {
+                httpclient.DefaultRequestHeaders.Clear();
+                httpclient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await httpclient.GetAsync("https://localhost:44301/api/Customer/get-cust-ById");
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response?.Content?.ReadAsStringAsync();
+                    var responseList = JsonConvert.DeserializeObject<GetAllCustResponse>(responseContent);
+                }
+            }
+
+            return Json(new { status = false });
+        }
+
+
+        [HttpGet]
+        [ActionName("Create-customer")]
+        public IActionResult CreateCust()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create-customer")]
+        public async Task<IActionResult> CreateCust(CreateCustRequest request)
+        {
+            CreateCustResponse customer = new CreateCustResponse();
+            using (var httpclient = new HttpClient(_clientHandler))
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                httpclient.DefaultRequestHeaders.Clear();
+                httpclient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await httpclient.PostAsync("https://localhost:44301/api/Customer/Create-customer",content);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [ActionName("Update-customer")]
+        public IActionResult UpdateCust()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Update-customer")]
+        public async Task <IActionResult> UpdateCust(UpdateCustRequest request)
+        {
+            
+            using (var httpclient = new HttpClient(_clientHandler))
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                httpclient.DefaultRequestHeaders.Clear();
+                httpclient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await httpclient.PostAsync("https://localhost:44301/api/Customer/Update-customer", content);
+                if(response.IsSuccessStatusCode) 
+                {
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+           
+        }
+
+        [HttpGet]
+        [ActionName("Delete-customer")]
+        public async Task<IActionResult>DeleteCust(int id)
+        {
+            CustomerModel request = new CustomerModel();
+            using (var httpclient = new HttpClient(_clientHandler))
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                httpclient.DefaultRequestHeaders.Clear();
+                httpclient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await httpclient.PostAsync("https://localhost:44301/api/Customer/Delete-customer?id="+id, content);
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        [ActionName("Get-joinlist")]
+        public async Task<ActionResult> Getjoinlist()
+        {
+
+            GetjoinlistResponse Customer = new GetjoinlistResponse();
+            using (var httpclient = new HttpClient(_clientHandler))
+            {
+                httpclient.DefaultRequestHeaders.Clear();
+                httpclient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await httpclient.GetAsync("https://localhost:44301/api/Customer/get-joinlist");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response?.Content?.ReadAsStringAsync();
+                    var responseList = JsonConvert.DeserializeObject<GetjoinlistResponse>(responseContent);
+
+                    Customer = responseList;
+                }
+            }
+            return View(Customer);
+        }
+    }
+}
---
+++ CrudClientApplication/Models/CustomerModel.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace CrudClientApplication.Models
+{
+    public class CustomerModel
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public int OrderId { get; set; }
+        public int OrderNumber { get; set; }
+        public string OrderDetials { get; set; }
+    }
+
+    public class GetAllCustResponse
+    {
+        public List<CustomerModel> CustomerList { get; set; }
+
+    }
+
+    public class GetAllCustRequest
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+       
+
+    }
+
+    public class GetCustByIdCustResponse 
+    {
+        public CustomerModel Customer { get; set; }
+    }
+
+    public class CreateCustResponse 
+    {
+        public int Customerid { get; set; }
+
+    }
+    public class CreateCustRequest 
+    {
+
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public int OrderNumber { get; set; }
+        public string OrderDetials { get; set; }
+
+    }
+
+    public class UpdateCustResponse 
+    {
+    }
+
+    public class UpdateCustRequest 
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+    }
+
+    public class DeleteCustResponse 
+    {
+        public int CustomerID { get; set; }
+    }
+    public class DeleteCustRequest 
+    {
+        public int CustomerID { get; set; }
+
+    }
+
+    public class GetjoinlistResponse
+    {
+        public List<CustomerModel> CustomerList { get; set; }
+    }
+
+}
